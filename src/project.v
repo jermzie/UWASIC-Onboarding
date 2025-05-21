ab2e85e98c56065cae81d49e716fa62968c21b02
jermzie <100226172+jermzie@users.noreply.github.com>
2025-05-21 13:56:50 +0000

    Fix spi instantiation - wrong input assignment

--- src/project.v
+++ src/project.v
@@ -21,7 +21,7 @@ module tt_um_uwasic_onboarding_jeremy_zheng (
   // All output pins must be assigned. If not used, assign to 0.
   assign uo_out  = out[7:0];
   assign uio_out = out[15:8];
-  assign uio_oe  = 8'hFF;
+  assign uio_oe  = 0;
 
   // Wires refer to the values of the registers
   wire [7:0] en_reg_out_7_0;
@@ -43,22 +43,22 @@ module tt_um_uwasic_onboarding_jeremy_zheng (
     .out(out)
   );
 
-  // List all unused inputs to prevent warnings
-  wire _unused = &{ena, ui_in[7:3], uio_oe, uio_in, 1'b0};
-
 
   // Initialize SPI module
   spi_peripheral spi_peripheral_inst (
     .clk(clk),
     .rst_n(rst_n),
-    .nCS(ui_in[0]),
+    .nCS(ui_in[2]),
     .COPI(ui_in[1]),
-    .SCLK(ui_in[2]),
+    .SCLK(ui_in[0]),
     .en_reg_out_7_0(en_reg_out_7_0),
     .en_reg_out_15_8(en_reg_out_15_8),
     .en_reg_pwm_7_0(en_reg_pwm_7_0),
     .en_reg_pwm_15_8(en_reg_pwm_15_8),
     .pwm_duty_cycle(pwm_duty_cycle)
   );
+
+  // List all unused inputs to prevent warnings
+  wire _unused = &{ena, uio_oe, ui_in[7:3], uio_in, 1'b0};
 
 endmodule
